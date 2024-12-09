9abdedb13b7b946b87d8c2c9543be0e9003168ec
jackie-ou <89358998+jackie-ou@users.noreply.github.com>
2024-12-08 21:56:58 -0800

    Add dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Table from "./Table";
+import json from "./data.json";
 
 function App() {
+  const columnHeaders = ["CHAMBER", "126", "127", "437", "444", "447", "1100", "Testing Conducted", "Notes"];
   return (
     <>
-      <Table />
+      <Table json={json} columnHeaders={columnHeaders} />
     </>
   );
 }
